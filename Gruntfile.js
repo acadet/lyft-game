11953ab625e7eeebaa26592b415edfcaaa21a511
Adrien Cadet <acadet@live.fr>
2015-07-25 17:52:44 -0700

    Prepares for a first release

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,6 +102,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task
-    //grunt.registerTask('default', []);
+    grunt.registerTask('default', ['coffee', 'sass', 'jade']);
 };
 
